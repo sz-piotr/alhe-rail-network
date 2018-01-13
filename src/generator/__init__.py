b38002cdefc2490398d30b01d2ba525296fe36ad
sz-piotr <szlachciak.piotr@gmail.com>
2018-01-13 16:45:47 +0100

    add problem generator

---
+++ src/generator/__init__.py
@@ -0,0 +1,34 @@
+from collections import namedtuple
+from .generateworld import generateworld
+
+REPEAT_COUNT = 5
+CITY_COUNTS = [5, 10, 20, 50]
+POWER_PLANT_RATIOS = [0, 0.2, 0.5, 0.7]
+U_VALUES = [0.05, 0.10, 0.15]
+T_VALUES = [1, 2, 5, 10]
+POPULATION_COUNTS = [10, 100, 1000]
+ITERATION_COUNTS = [100, 1000, 10000, 1000000]
+
+Problem = namedtuple('Problem', ['U', 'T', 'population', 'iterations', 'world'])
+
+def generate_problems():
+    worlds = [
+        generateworld(n_cities, 1 + powerplant_ratio * n_cities)
+        for i in range(REPEAT_COUNT)
+        for n_cities in CITY_COUNTS
+        for powerplant_ratio in POWER_PLANT_RATIOS
+    ]
+    return [
+        problem
+        for world in worlds
+        for problem in generate_problems_with(world)
+    ]
+
+def generate_problems_with(world):
+    return [
+        Problem(U, T, population, iterations, world)
+        for U in U_VALUES
+        for T in T_VALUES
+        for population in POPULATION_COUNTS
+        for iterations in ITERATION_COUNTS
+    ]
