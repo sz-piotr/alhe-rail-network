2edfbcecd9ce90de73f56278ab2ef8ba019e58ce
sz-piotr <szlachciak.piotr@gmail.com>
2018-01-13 13:46:19 +0100

    add railnetwork.py

---
+++ src/railnetwork.py
@@ -0,0 +1,25 @@
+import networkx as nx
+
+class RailNetwork:
+
+    __init__(self):
+        self.graph = nx.Graph()
+
+    def score(self, powerplants):
+        return 0
+
+    def crossover(self, other):
+        full_child = nx.Graph()
+        full_child.add_edges_from(self.graph)
+        full_child.add_edges_from(other.graph)
+        mst = nx.minimum_spanning_edges(full_child, 'weight')
+
+        child = nx.Graph()
+        child.add_edges_from(list(mst))
+
+        return child
+
+    def mutate(self)
+        mutant = nx.Graph()
+        mutant.add_edges_from(self.graph)
+        return mutant
