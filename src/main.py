835f1c673097b82de9d0f31e59712c95a1e20fb5
ytrone <kubagar@gmail.com>
2018-01-13 18:08:28 +0100

    Population generator

--- src/main.py
+++ src/main.py
@@ -3,20 +3,21 @@ from fitness import fitness
 from selection import selection
 from crossover import crossover
 from mutation import mutation
+import populationgenerator
 import loadconfiguration
 
 def main():
+    path = '../src/config'
+    testconfig = loadconfiguration.loadConfig(path)
+    points = loadconfiguration.loadPoints(path)
+
     evolution = Evolution(
-        list(range(10)),
+        populationgenerator.generate_population(populationgenerator.generate_single_tree(points), 10),
         fitness,
         selection,
         crossover,
         mutation
     )
-    #path = '../src/config'
-    #testconfig = loadconfiguration.loadConfig(path)
-    #points = loadconfiguration.loadPoints(path)
-
 
     print(evolution.population)
     for i in range(100):
