3c56304635ff3265f3eab2842261251b840c8981
sz-piotr <szlachciak.piotr@gmail.com>
2018-01-14 12:28:44 +0100

    fix crossover

--- src/crossover.py
+++ src/crossover.py
@@ -2,13 +2,12 @@ import random
 import networkx as nx
 
 def crossover(graph1, graph2):
-
     g = nx.Graph()
-    g.add_edges_from(graph1)
-    g.add_edges_from(graph2)
+    g.add_edges_from(graph1.edges)
+    g.add_edges_from(graph2.edges)
 
-    for (u, v) in g.edges():
-        g.edge[u][v]['weight'] = random.uniform(0,10)
+    for (u, v) in g.edges:
+        g.edges[u, v]['weight'] = random.uniform(0,10)
 
-    return nx.minimum_spanning_tree(g, 'weight')
+    return nx.minimum_spanning_tree(g)
 
