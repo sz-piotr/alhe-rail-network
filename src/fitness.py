5c202a29ba6111c55a96c65aad69218bbfbcf457
sz-piotr <szlachciak.piotr@gmail.com>
2018-01-14 12:07:04 +0100

    fix fitness bugs

--- src/fitness.py
+++ src/fitness.py
@@ -1,14 +1,14 @@
 import math
 
 def makefitness(U, T, cities):
-    powerplants = filter(lambda city: city.has_powerplant, cities)
+    powerplants = list(filter(lambda city: city.has_powerplant, cities))
     electicity_costs = [electicity_cost(city, powerplants, U) for city in cities]
 
     def fitness(specimen):
         sum = 0
         for v1, v2 in specimen.edges():
             distance_penalty = distance(cities[v1], cities[v2]) * T
-            min_cost = min(electicity_costs[v1], electicity_cost[v2])
+            min_cost = min(electicity_costs[v1], electicity_costs[v2])
             sum += distance_penalty + min_cost
         return sum
 
@@ -16,7 +16,7 @@ def makefitness(U, T, cities):
 
 def electicity_cost(city, powerplants, U):
     min_distance = min([distance(city, plant) for plant in powerplants])
-    return distance ** (1 + U)
+    return min_distance ** (1 + U)
 
 
 def distance(city, other):
